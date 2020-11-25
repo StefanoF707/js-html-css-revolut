c365bf8a4f7e4967f0296a05cfef006a45102e4a
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-25 17:54:49 +0100

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
+$(document).ready(
+    function () {
 
+        var links = $("#header-right li > a");
+        var cascadingMenu = $(".cascading-menu");
+
+        // links.hover(
+        //     function () {
+        //         $(this).css("color", "black");
+        //         cascadingMenu.css("display", "none");
+        //         $(this).next().toggle(200);
+        //     }, function () {
+        //         $(this).css("color", "inherit");
+        //     }
+        // );
+
+        links.mouseenter(
+            function() {
+                $(this).css("color", "black");
+                cascadingMenu.css("display", "none");
+                $(this).next().toggle(200);
+            }
+        );
+
+        links.mouseleave(
+            function() {
+                $(this).css("color", "inherit");
+            }
+        );
+
+        links.click(
+            function () {
+                $(this).next().toggle(200);
+            }
+        );
+
+
+
+
+
+
+    }
+);
