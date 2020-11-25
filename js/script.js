201b9465a8e665da50fda77f4a86ce6bd275655b
Stefano Franchini <stefano.franchini77@gmail.com>
2020-11-25 23:35:09 +0100

    refactoring

--- js/script.js
+++ js/script.js
@@ -3,21 +3,14 @@ $(document).ready(
 
         var links = $("#header-right li > a");
         var cascadingMenu = $(".cascading-menu");
-        var cascadingMenuLinks = $(".cascading-menu a")
-
-        links.click(
-            function () {
-                $(this).next().toggle(200);
-            }
-        );
 
         links.mouseenter(
             function() {
                 $(this).css("color", "#000");
                 $(this).next().toggle(200,
                     function () {
-                        cascadingMenu.css("display", "none");
-                        $(this).css("display", "block");
+                        cascadingMenu.hide();
+                        $(this).show();
                     }
                 );
             }
@@ -26,6 +19,12 @@ $(document).ready(
         links.mouseleave(
             function () {
                 $(this).css("color", "inherit");
+            }
+        );
+
+        links.click(
+            function () {
+                $(this).next().toggle(200);
             }
         );
 
